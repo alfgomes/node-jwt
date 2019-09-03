ea76ddb4585f3dd78c68539dbe425ba885484c82
André Gomes <andre.gomes@bee-eng.pt>
2019-09-03 15:18:14 +0100

    Código Inicial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+app.get('/api', (req, res) =>{
+    res.json({
+        message:'Welcome to the API'
+    })
+})
+
+app.post('/api/posts', (req, res) =>{
+    res.json({
+        message:'Post successfully created'
+    })
+})
+
+app.listen(5000,
+    () => {
+        console.clear();
+        console.log('Server started on Port 5000');
+    }
+)
